9300f05579873c00c7a259b1b9543c2f35ee3d55
nomadjesse93 <jesse93@protonmail.com>
2019-12-24 13:48:50 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+import '../App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header text='This Is the Header' />
+    </div>
+  );
+}
+
+export default App;
